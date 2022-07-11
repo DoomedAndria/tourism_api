796978b996dda94c94cfd79c578db93fec113796
Andria Bibiashvili <103760839+DoomedAndria@users.noreply.github.com>
2022-07-12 03:16:29 +0400

    Update index.js

--- index.js
+++ index.js
@@ -18,34 +18,34 @@ app.use(bodyparser.urlencoded({extended:true}))
 
 
 //insert user into database
-app.post('/api/users/insert',(req,res)=>{
-    const name = req.body.name
-    const surname = req.body.surname
-    const phone_number = req.body.phone_number
-    const email = req.body.email
-    const password = req.body.password
+// app.post('/api/users/insert',(req,res)=>{
+//     const name = req.body.name
+//     const surname = req.body.surname
+//     const phone_number = req.body.phone_number
+//     const email = req.body.email
+//     const password = req.body.password
 
-    const sqlInsert = "INSERT INTO user (name,surname,phone_number,email,password) VALUES (?,?,?,?,?)"
-    db.query(sqlInsert,[name,surname,phone_number,email,password],(err,result)=>{
-        console.log(result)
-    })
-})
+//     const sqlInsert = "INSERT INTO user (name,surname,phone_number,email,password) VALUES (?,?,?,?,?)"
+//     db.query(sqlInsert,[name,surname,phone_number,email,password],(err,result)=>{
+//         console.log(result)
+//     })
+// })
 
-//fetching card information
-app.get('/api/cards',(req,res)=>{
-    const sqlFetch = "SELECT * FROM card"
-    db.query(sqlFetch,(err,result)=>{
-        res.send(result)
-    })
-})
+// //fetching card information
+// app.get('/api/cards',(req,res)=>{
+//     const sqlFetch = "SELECT * FROM card"
+//     db.query(sqlFetch,(err,result)=>{
+//         res.send(result)
+//     })
+// })
 
-// fetching gallery information
-app.get('/api/gallery',(req,res)=>{
-    const sqlFetch = "SELECT * FROM galery"
-    db.query(sqlFetch,(err,result)=>{
-        res.send(result)
-    })
-})
+// // fetching gallery information
+// app.get('/api/gallery',(req,res)=>{
+//     const sqlFetch = "SELECT * FROM galery"
+//     db.query(sqlFetch,(err,result)=>{
+//         res.send(result)
+//     })
+// })
 
 app.get('/',(req,res)=>{
     res.send('hello')
